618d3122d49407d693dd0354ed7a12a3fc7b173c
sergicastellasape <castella.sergi@gmail.com>
2020-02-13 15:48:54 +0100

    job

--- job.sh
+++ job.sh
@@ -16,7 +16,7 @@ module load 2019
 module load CUDA/10.1.243
 module load Python/3.7.5-foss-2018b
 
-cp -r "$HOME"/semantic-compression/ "$TMPDIR"
+cp -r $HOME/semantic-compression/ $TMPDIR
 
 # shellcheck disable=SC2164
 cd $TMPDIR/semantic-compression/
@@ -27,7 +27,7 @@ cp "output_${RUN_NAME}.txt" "$HOME"/semantic-compression/outputs/
 
 RUN_NAME="comp$(date +%d_%m_%Y_%H_%M_%S)";
 python3 scripts/comp_training.py --run-identifier ${RUN_NAME} -thr 0.8 --tensorboard-dir tensorboard --eval-periodicity 50 --wall-time 800 >> "output_${RUN_NAME}.txt"
-cp "output_${RUN_NAME}.txt" "$HOME"/semantic-compression/outputs/
+cp output_${RUN_NAME}.txt $HOME/semantic-compression/outputs/
 
 cp -r $TMPDIR/semantic-compression/tensorboard/ $HOME/semantic-compression
 cp -r $TMPDIR/semantic-compression/checkpoints/ $HOME/semantic-compression
