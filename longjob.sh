88e384ca9d3bfb0073a89fde44c7e16c75a90033
sergicastellasape <castella.sergi@gmail.com>
2020-02-13 13:30:59 +0100

    longjob

--- longjob.sh
+++ longjob.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 #SBATCH --job-name=trainint_test
 #SBATCH --nodes=1
-#SBATCH --time=6:00:00
+#SBATCH --time=5:00:00
 #SBATCH --cpus-per-task=8
 #SBATCH --ntasks-per-node=1
 #SBATCH --gres=gpu:1
