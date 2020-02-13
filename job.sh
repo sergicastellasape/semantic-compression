24c777b1470a2a3198fa5955d94dcf1734b32c9e
sergicastellasape <castella.sergi@gmail.com>
2020-02-13 12:08:22 +0100

    job

--- job.sh
+++ job.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 #SBATCH --job-name=trainint_test
 #SBATCH --nodes=1
-#SBATCH --time=4:00:00
+#SBATCH --time=0:25:00
 #SBATCH --cpus-per-task=8
 #SBATCH --ntasks-per-node=1
 #SBATCH --gres=gpu:1
@@ -9,7 +9,6 @@
 #SBATCH --mem=16G
 #SBATCH --mail-type=END
 #SBATCH --mail-user=castella.sergi@gmail.com
-
 
 
 module purge
@@ -20,15 +19,15 @@ module load Python/3.7.5-foss-2018b
 cp -r "$HOME"/semantic-compression/ "$TMPDIR"
 
 # shellcheck disable=SC2164
-cd "$TMPDIR"/semantic-compression/
+cd $TMPDIR/semantic-compression/
 source venv/bin/activate
 RUN_NAME="base$(date +%d_%m_%Y_%H_%M_%S)";
-python3 scripts/base_training.py --run-identifier ${RUN_NAME} -thr 1 --tensorboard-dir tensorboard --eval-periodicity 50 --wall-time 5000 >> "output_${RUN_NAME}.txt"
-cp "output_${RUN_NAME}.txt" "$HOME"/semantic-compression/outputs
+python3 scripts/base_training.py --run-identifier ${RUN_NAME} -thr 1 --tensorboard-dir tensorboard --eval-periodicity 50 --wall-time 500 >> "output_${RUN_NAME}.txt"
+cp "output_${RUN_NAME}.txt" "$HOME"/semantic-compression/outputs/
 
 RUN_NAME="comp$(date +%d_%m_%Y_%H_%M_%S)";
-python3 scripts/comp_training.py --run-identifier ${RUN_NAME} -thr 0.8 --tensorboard-dir tensorboard --eval-periodicity 50 --wall-time 8000 >> "output_${RUN_NAME}.txt"
-cp "output_${RUN_NAME}.txt" "$HOME"/semantic-compression/outputs
+python3 scripts/comp_training.py --run-identifier ${RUN_NAME} -thr 0.8 --tensorboard-dir tensorboard --eval-periodicity 50 --wall-time 800 >> "output_${RUN_NAME}.txt"
+cp "output_${RUN_NAME}.txt" "$HOME"/semantic-compression/outputs/
 
-cp -r "tensorboard/" "$HOME"/semantic-compression
-cp -r "checkpoints/" "$HOME"/semantic-compression
+cp -r $TMPDIR/semantic-compression/tensorboard/ $HOME/semantic-compression
+cp -r $TMPDIR/semantic-compression/checkpoints/ $HOME/semantic-compression
