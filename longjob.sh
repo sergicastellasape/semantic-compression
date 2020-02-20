e9fcd1d0e958126e6fb7ac2d7b5c2ea7b16d552f
sergicastellasape <castella.sergi@gmail.com>
2020-02-20 16:48:16 +0100

    add longjob to remove

--- longjob.sh
+++ longjob.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
-#SBATCH --job-name=big_memory_run
+#SBATCH --job-name=big-run
 #SBATCH --nodes=1
-#SBATCH --time=5:00:00
+#SBATCH --time=7:00:00
 #SBATCH --cpus-per-task=4
 #SBATCH --ntasks-per-node=1
 #SBATCH --partition=gpu
-#SBATCH --mem=40G
+#SBATCH --mem=32G
 #SBATCH --mail-type=END
 #SBATCH --mail-user=castella.sergi@gmail.com
 
@@ -20,13 +20,22 @@ cp -r $HOME/semantic-compression/ $TMPDIR
 # shellcheck disable=SC2164
 cd $TMPDIR/semantic-compression/
 source venv/bin/activate
-RUN_NAME="base$(date +%d_%m_%Y_%H_%M_%S)";
-python3 scripts/base_training.py --run-identifier ${RUN_NAME} -thr=1 -lr=0.0001 --tensorboard-dir tensorboard --eval-periodicity 50 --wall-time 6000 >> outputs/output_${RUN_NAME}.txt
-cp output_${RUN_NAME}.txt $HOME/semantic-compression/outputs/
 
-RUN_NAME="comp$(date +%d_%m_%Y_%H_%M_%S)";
-python3 scripts/comp_training.py --run-identifier ${RUN_NAME} -thr=0.8  -lr=0.0001 --tensorboard-dir tensorboard --eval-periodicity 50 --wall-time 9000 >> outputs/output_${RUN_NAME}.txt
-cp output_${RUN_NAME}.txt $HOME/semantic-compression/outputs/
+RUN_NAME="base-$(date +%d_%m_%Y_%H_%M_%S)";
+python3 scripts/base_training.py --run-identifier ${RUN_NAME} -thr=1 -lr=0.0001 --tensorboard-dir tensorboard --eval-periodicity 50 --wall-time 3000 --train-compression False --eval-compression False >> outputs/output_${RUN_NAME}.txt
+cp outputs/output_${RUN_NAME}.txt $HOME/semantic-compression/outputs/
+
+RUN_NAME="comp-09-$(date +%d_%m_%Y_%H_%M_%S)";
+python3 scripts/base_training.py --run-identifier ${RUN_NAME} -thr=0.9  -lr=0.0001 --tensorboard-dir tensorboard --eval-periodicity 50 --wall-time 7000 --train-compression True --eval-compression True >> outputs/output_${RUN_NAME}.txt
+cp outputs/output_${RUN_NAME}.txt $HOME/semantic-compression/outputs/
+
+RUN_NAME="comp-08-$(date +%d_%m_%Y_%H_%M_%S)";
+python3 scripts/base_training.py --run-identifier ${RUN_NAME} -thr=0.8  -lr=0.0001 --tensorboard-dir tensorboard --eval-periodicity 50 --wall-time 7000 --train-compression True --eval-compression True >> outputs/output_${RUN_NAME}.txt
+cp outputs/output_${RUN_NAME}.txt $HOME/semantic-compression/outputs/
+
+RUN_NAME="comp-07-$(date +%d_%m_%Y_%H_%M_%S)";
+python3 scripts/base_training.py --run-identifier ${RUN_NAME} -thr=0.7  -lr=0.0001 --tensorboard-dir tensorboard --eval-periodicity 50 --wall-time 7000 --train-compression True --eval-compression True >> outputs/output_${RUN_NAME}.txt
+cp outputs/output_${RUN_NAME}.txt $HOME/semantic-compression/outputs/
 
 cp -r $TMPDIR/semantic-compression/tensorboard/ $HOME/semantic-compression
-cp -r $TMPDIR/semantic-compression/assets/checkpoints/ $HOME/semantic-compression
+cp -r $TMPDIR/semantic-compression/assets/checkpoints/ $HOME/semantic-compression/assets
